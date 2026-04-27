2249a0f5fa082a4b1cc10e99d6fb9a292aef2a83
AlanGomez-Programmer <Alan.s.g.j.2006@gmail.com>
2026-04-26 19:54:30 -0600

    feat: :sparkles: Se agrega la función agregar producto

---
+++ inventario.py
@@ -0,0 +1,33 @@
+from json import load, dumps
+
+ruta_inventario = "inventario.json"
+
+def leer_archivo(ruta):
+    with open(ruta, "r") as archivo:
+        datos = load(archivo)
+        return datos
+   
+
+def guardar_datos(ruta, datos):
+    with open(ruta, "w") as archivo:
+        archivo.write(dumps(datos, indent=4))
+   
+
+
+def agregar_productos():
+
+    productos = leer_archivo(ruta_inventario)
+
+    nombre = input("PRODUCTO: ").strip()
+    precio = float(input("PRECIO: ").strip())
+    cantidad = int(input("CANTIDAD: ").strip())
+
+    producto = {
+        "precio": precio,
+        "cantidad": cantidad
+    }
+
+    productos[nombre] = producto
+
+    guardar_datos(ruta_inventario, productos)
+    print("Producto agregado correctamente...")
