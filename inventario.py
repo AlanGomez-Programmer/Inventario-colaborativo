dad48e653b7d3235a4fbf2ba1f85c0858f259269
AlanGomez-Programmer <Alan.s.g.j.2006@gmail.com>
2026-04-26 20:08:08 -0600

    feat: :fire: Se removio la importaciones de json

--- inventario.py
+++ inventario.py
@@ -1,4 +1,4 @@
-from json import load, dumps
+
 
 ruta_inventario = "inventario.json"
 
@@ -46,3 +46,5 @@ def listar_productos():
             print(f"Producto : {nombre}")
             print(f"Precio : {precio}")
             print(f"Cantidad : {cantidad}")
+
+print("ERROR EN INVENTARIO")
